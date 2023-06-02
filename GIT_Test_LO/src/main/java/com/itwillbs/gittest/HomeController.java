0381c604ac886689bcc12a902ccba90293fc7563
user <user@ITWILL>
2023-06-02 11:16:33 +0900

    홈콘츄롤라 주석추가 ㅎㅎㅎㅎ

--- GIT_Test_LO/src/main/java/com/itwillbs/gittest/HomeController.java
+++ GIT_Test_LO/src/main/java/com/itwillbs/gittest/HomeController.java
@@ -32,7 +32,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute("serverTime", formattedDate );
-		//뚜쉬
+		//뚜쉬ㅎㅎㅎㅎㅎ
 		return "home";
 	}
 	
